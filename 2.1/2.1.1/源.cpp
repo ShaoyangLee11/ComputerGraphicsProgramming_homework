5c5e6a0148a4b83c866bc5df5a58dbedfa2cd4ad
d4v1d_lee <1278017085@qq.com>
2025-11-18 12:02:23 +0800

    添加项目文件。

---
+++ 2.1/2.1.1/源.cpp
@@ -0,0 +1,39 @@
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
+
+using namespace std;
+
+//本题只提供代码框架，有些函数并未写完整是因为在此题中没有用到
+
+void init(GLFWwindow* window){}
+
+void display(GLFWwindow* window, double currentTime) {
+	glClearColor(1.0, 0.0, 0.0, 1.0);//指定清楚背景时所用的颜色，这里使用红色
+	glClear(GL_COLOR_BUFFER_BIT);//清理颜色缓冲区中的数据，并用红色填充颜色缓冲区
+}
+
+int main(){
+	//GLFW_part
+	if (!glfwInit()) { exit(EXIT_FAILURE); }//初始化失败直接exit
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//指定GLFW的版本
+	GLFWwindow* window1 = glfwCreateWindow(600, 600, "Chapter2-Program1", NULL,NULL);//创建这个window的OpenGL属性（也称作Context,上下文）
+	glfwMakeContextCurrent(window1);//将新创建的窗口window1与他的上下文关联起来，window与context不会自动关联，必须调用此函数
+
+	//GLEW_part
+	if(glewInit()!=GLEW_OK){ exit(EXIT_FAILURE); }
+	glfwSwapInterval(1);//开启垂直同步，防止撕帧
+
+	init(window1);
+
+	while (!glfwWindowShouldClose(window1)) {//检测窗口关闭事件
+		display(window1, glfwGetTime());//glfwGetTime会返回GLFW初始化之后经过的时间
+		glfwSwapBuffers(window1);//交换缓冲区
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window1);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
