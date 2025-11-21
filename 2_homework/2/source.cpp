dfced8e0b2de0ae6595a31e0e1dc1853194f6ec5
d4v1d_lee <1278017085@qq.com>
2025-11-21 13:31:36 +0800

    Add new folder 2 in folder 2_homework

---
+++ 2_homework/2/source.cpp
@@ -0,0 +1,99 @@
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+#define numVAOs 1
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+string readShaderSource(const char* filePath) {
+	string content;
+	ifstream fileStream(filePath, ios::in);
+
+	if (!fileStream.is_open()) {
+		cerr << "Could not read file " << filePath << ". File does not exist." << endl;
+		return "";
+	}
+
+	string line = "";
+	while (!fileStream.eof()) {
+		getline(fileStream, line);
+		content.append(line + "\n");
+	}
+
+	fileStream.close();
+	return content;
+}
+
+GLuint createShaderProgram() {
+
+	string vShaderStr = readShaderSource("vertShader.glsl");
+	string fShaderStr = readShaderSource("fragShader.glsl");
+
+	const char* vShaderSource = vShaderStr.c_str();
+
+	const char* fShaderSource = fShaderStr.c_str();
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vShader, 1, &vShaderSource, NULL);
+	glShaderSource(fShader, 1, &fShaderSource, NULL);
+
+	glCompileShader(vShader);
+	glCompileShader(fShader);
+
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+	glLinkProgram(vfProgram);
+
+	return vfProgram;
+}
+
+void init(GLFWwindow* window) {
+	renderingProgram = createShaderProgram();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(renderingProgram);
+
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+using namespace std;
+
+int main() {
+	//GLFW_part
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window1 = glfwCreateWindow(600, 600, "Chapter2-Program1", NULL, NULL);
+	glfwMakeContextCurrent(window1);
+
+	//GLEW_part
+	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+	glfwSwapInterval(1);
+
+	init(window1);
+
+	while (!glfwWindowShouldClose(window1)) {
+		display(window1, glfwGetTime());
+		glfwSwapBuffers(window1);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window1);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
