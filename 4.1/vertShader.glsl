e19165acdf3fbe8573a3aee45f44cef6c1887ca5
d4v1d_lee <1278017085@qq.com>
2025-11-25 20:30:05 +0800

    Add new folders for repository

---
+++ 4.1/vertShader.glsl
@@ -0,0 +1,12 @@
+#version 430
+layout(location=0) in vec3 Position;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+
+out vec4 varyingColor;
+
+void main(void){
+	gl_Position =proj_matrix* mv_matrix * vec4(Position,1.0);
+	varyingColor = vec4(Position,1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5);
+}
